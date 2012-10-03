e5248cb96209555d43cd523c6838e2fdd0c8a70b
Diederik van Liere <dvanliere@wikimedia.org>
2012-10-03 18:44:50 +0000

    Updated bash tests, some are still failing

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ make DEBUG=1
 ip_filter1=$(cat example.log | ./udp-filter -i 216.38.130.161 | wc -l)
 ip_filter2=$(cat example.log | ./udp-filter -i 0.0.0.0/0 | wc -l)
 ip_filter3=$(cat example.log | ./udp-filter -i 216.38.130.161,0.0.0.0 | wc -l)
-ip_filter4=$(cat example.log | ./udp-filter -i 127.0.0.1-192.168.0.0,216.0.0.0-255.0.0.0 | wc -l)
-ip_filter5=$(cat example.collector.log | ./udp-filter -o -B | diff example.collector.expected_result -)
 ip_filter4=$(cat example.log | ./udp-filter -i 127.0.0.1/14,216.0.0.0/4 | wc -l)
+ip_filter5=$(cat example.collector.log | ./udp-filter -o -B | diff example.collector.expected_result -)
+
 
 domain_filter1=$(cat example.log | ./udp-filter -d waka | wc -l)
 domain_filter2=$(cat example.log | ./udp-filter -d wiki -g -c US -b country | wc -l)
@@ -30,8 +30,10 @@ red="\033[31m"
 green="\033[32m"
 black="\033[30m"
 
-alias Reset="tput sgr0"      #  Reset text attributes to normal
 
+alias Reset="tput sgr0"      #  Reset text attributes to normal
+ 
+ 
 cecho ()                     # Color-echo.
                              # Argument $1 = message
                              # Argument $2 = color
@@ -140,13 +142,14 @@ fi
 if [ -z "$ip_filter5" ]; then
   cecho "Pass" $green
 else
-  cecho "Fail" $red
+	cecho "Fail" $red
 fi
+
 
 if [ $referer_filter1 -eq 1 ]; then
 	cecho "Pass" $green
 else
 	cecho "Fail" $red
 fi
-
 exit 0
+
